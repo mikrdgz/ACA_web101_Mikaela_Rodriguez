89ab24844ef805f1d47a996a7cf001a9dc6468f7
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-03-16 16:53:14 -0500

    adding geolocation

---
+++ myPortfolio/dashboard-grid/dashboard.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+    let x = document.getElementById('geo');
+
+   navigator.geolocation.getCurrentPosition(showPosition);
+    function showPosition(position) {
+        x.innerHTML = "<strong> Your latitude: </strong>" + position.coords.latitude + 
+        "<br><strong>Your longitude: </strong>" + position.coords.longitude; 
+      }
+
+
+
+  }
