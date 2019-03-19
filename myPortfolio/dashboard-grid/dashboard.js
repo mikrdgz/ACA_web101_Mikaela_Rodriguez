6eef61d8a678db2212f5c02c55c010a057c79b80
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-03-19 17:09:54 -0500

    add jq to dash

--- myPortfolio/dashboard-grid/dashboard.js
+++ myPortfolio/dashboard-grid/dashboard.js
@@ -1,17 +1,37 @@
+
+
 window.onload = function() {
 
-    let x = document.getElementById('geo');
+  let x = document.getElementById('geo');
 
-   navigator.geolocation.getCurrentPosition(showPosition);
-    function showPosition(position) {
-        x.innerHTML = "<strong> Your latitude: </strong>" + position.coords.latitude + 
-        "<br><strong>Your longitude: </strong>" + position.coords.longitude; 
-      }
-
+ navigator.geolocation.getCurrentPosition(showPosition);
+  function showPosition(position) {
+      x.innerHTML = "<strong> Your latitude: </strong>" + position.coords.latitude + 
+      "<br><strong>Your longitude: </strong>" + position.coords.longitude; 
     }
 
-const helloFunction = () => console.log('hi')
+  }
 
+  $(document).ready(function(){
 
+    let list = $('ol');
+    let newListForm = $('#newListForm');
+    
+    $(newListForm).submit(function(event){
+    let text = $('input[type=text]').val();
+        event.preventDefault();
+        $(list).append('<li>' + text + '</li>')
+        });
+    
+        list.on('click', 'li', function() {
+            let $this = $(this);
+            console.log(this);
+            $(this).remove();
+          });
+    
+    });
+    
+    
+      
 
 
